2df5e7d37f2c2add908895e4014f1c0814adbd4d
Tobias Wallin <tobias.wallin@entiros.se>
2015-08-12 19:03:50 +0200

    fixed peer removal pattern

--- factorio-logparser.py
+++ factorio-logparser.py
@@ -133,7 +133,7 @@ regex = {}
 
 # Synchronizer.cpp
 regex[0] = {}
-regex[0][0] = re.compile("Synchronizer.cpp")
+regex[0][0] = re.compile("NetworkInputHandler.cpp")
 regex[0]['removepeer'] = Processor("removing peer\\((?P<peer_id>\d+)\\) success\\(true\\)", Server.remove_peer)
 # Router.cpp
 regex[1] = {}
