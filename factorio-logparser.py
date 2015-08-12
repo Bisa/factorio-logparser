516350ad1be8baba30511b6ac019f180b416bcf9
Tobias Wallin <tobias.wallin@entiros.se>
2015-08-12 19:06:03 +0200

    fixed hardcoded filename

--- factorio-logparser.py
+++ factorio-logparser.py
@@ -13,7 +13,7 @@ import os
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument('logfile', nargs=1, help="absolute path to factorio-current.log")
+parser.add_argument('logfile', help="absolute path to factorio-current.log")
 parser.add_argument('-o', '--outputfile', help="absolute path to status output file")
 parser.add_argument('-f', '--statusfrequency', type=float, help="frequency in seconds for reporting status")
 options = parser.parse_args()
@@ -121,7 +121,7 @@ def report_status():
 
 signal.signal(signal.SIGINT, signal_handler)
 
-filename = "/opt/factorio/factorio-current.log"
+filename = options.logfile
 threading.Thread(target=tail_forever, args=(filename,)).start()
 report_status()
 
