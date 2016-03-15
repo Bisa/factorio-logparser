7f14d96247e174afc70bec8cd702f06823b71ae4
Tobias Wallin <gitcommit@tobiaswallin.info>
2016-03-15 16:50:20 +0000

    more structuring

--- flogparser/flogparser.py
+++ flogparser/flogparser.py
@@ -1,65 +1,45 @@
-import time
-import sys
-import threading
-import signal
-import traceback
-import logging
 import Queue
+import threading
+import time
+import tailhead
+import logging
 
-import tailer
-import parser
+class Tailer(threading.Thread):
+    def __init__(self, filename, stop_flag, tailqueue):
+        super(Tailer, self).__init__()
 
-class InterruptHandler(object):
-    def __init__(self, sig=signal.SIGINT):
-        self.sig = sig
-        
-    def __enter__(self):
-        self.interrupted = threading.Event()
-        self.released = threading.Event()
-        self.original_handler = signal.getsignal(self.sig)
-        def handler(signum, frame):
-            self.release()
-            self.interrupted.set()
-        signal.signal(self.sig, handler)
-        return self
-        
-    def __exit__(self, type, value, tb):
-        self.release()
-        
-    def release(self):
-        if self.released.is_set():
-            return False
-        signal.signal(self.sig, self.original_handler)
-        self.released.set()
-        return True
+        self.filename = filename
+        self.stop_flag = stop_flag
+        self.tailqueue = tailqueue
 
-def main(fn):
-    logging.basicConfig(level=logging.DEBUG)
-
-    with InterruptHandler() as h:
-        try:
-            logging.info("#### STARTING ####")
-            tailqueue = Queue.Queue()
-            producer = tailer.Tailer(fn,h.interrupted,tailqueue)
-            consumer = parser.Parser(h.interrupted,tailqueue)
-        
-            producer.start()
-            consumer.start()
-
-            # Wait for SIGINT/TERM
-            while not h.interrupted.is_set():
+    def run(self):
+        logging.info("#### TAILING: " + self.filename +  " ####")
+        for line in tailhead.follow_path(self.filename):
+            # Stop tailing
+            if self.stop_flag.is_set():
+                break
+            # Enqueue lines or sleep till we have a new line
+            if line is not None:
+                self.tailqueue.put(line)
+            else:
                 time.sleep(0.5)
 
-            logging.info("#### SHUTTING DOWN ####")
-            producer.join()
-            consumer.join()
+class Parser(threading.Thread):
 
-        except Exception:
-            logging.error(traceback.format_exc())
-            sys.exit(1)
-        finally:
-            logging.info("#### FLOGPARSER TERMINATED ####")
-        sys.exit(0)
+    def __init__(self, stop_flag, tailqueue):
+        super(Parser, self).__init__()
+        self.stop_flag = stop_flag
+        self.tailqueue = tailqueue
 
-if __name__ == "__main__":
-    main('/tmp/foo')
+    def run(self):
+        logging.info("#### PARSING: Inititated ####")
+        while True:
+           try:
+               #TODO: something useful 8implement listeners?)
+               print(self.tailqueue.get_nowait())
+           except  Queue.Empty:
+               # No more messages on queue, stop processing?
+               if self.stop_flag.is_set():
+                   logging.info("#### STOPPING PARSER ####")
+                   break
+               pass
---
+++ flogparser/main.py
@@ -0,0 +1,64 @@
+import time
+import sys
+import threading
+import signal
+import traceback
+import logging
+import Queue
+
+import flogparser
+
+class InterruptHandler(object):
+    def __init__(self, sig=signal.SIGINT):
+        self.sig = sig
+        
+    def __enter__(self):
+        self.interrupted = threading.Event()
+        self.released = threading.Event()
+        self.original_handler = signal.getsignal(self.sig)
+        def handler(signum, frame):
+            self.release()
+            self.interrupted.set()
+        signal.signal(self.sig, handler)
+        return self
+        
+    def __exit__(self, type, value, tb):
+        self.release()
+        
+    def release(self):
+        if self.released.is_set():
+            return False
+        signal.signal(self.sig, self.original_handler)
+        self.released.set()
+        return True
+
+def main(fn):
+    logging.basicConfig(level=logging.DEBUG)
+
+    with InterruptHandler() as h:
+        try:
+            logging.info("#### STARTING ####")
+            tailqueue = Queue.Queue()
+            tailer = flogparser.Tailer(fn,h.interrupted,tailqueue)
+            parser = flogparser.Parser(h.interrupted,tailqueue)
+        
+            parser.start()
+            tailer.start()
+
+            # Wait for SIGINT/TERM
+            while not h.interrupted.is_set():
+                time.sleep(0.5)
+
+            logging.info("#### SHUTTING DOWN ####")
+            producer.join()
+            consumer.join()
+
+        except Exception:
+            logging.error(traceback.format_exc())
+            sys.exit(1)
+        finally:
+            logging.info("#### FLOGPARSER TERMINATED ####")
+        sys.exit(0)
+
+if __name__ == "__main__":
+    main('/tmp/foo')
