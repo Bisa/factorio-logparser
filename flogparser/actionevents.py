e58389d3a6b45a97d072e75087920a0350d002d3
Tobias Wallin <gitcommit@tobiaswallin.info>
2016-03-15 22:25:21 +0000

    actual event classes

---
+++ flogparser/actionevents.py
@@ -0,0 +1,33 @@
+
+class PeerAdded():
+    def __init__(self, args):
+        super(self)
+
+class PeerDesynced():
+    def __init__(self, args):
+        super(self)
+
+class PeerUserName():
+    def __init__(self, args):
+        super(self)
+
+class PeerRemoved():
+    def __init__(self, args):
+        super(self)
+
+class PeerDesynced():
+    def __init__(self, args):
+        super(self)
+
+class PeerPlayerIndex():
+    def __init__(self, args):
+        super(self)
+
+class ServerStarted():
+    def __init__(self, args):
+        super(self)
+
+class ServerStopped():
+    def __init__(self, args):
+        super(self)
+
